4bf84c1b1ace563fe7ab576f5a15aa959c370528
Alejandro Ohrnialian Bengolea <aob@live.com.ar>
2022-08-02 19:23:39 -0300

    java bien

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,30 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  var casa = "hola"
+  var primera = "hola"
+  var acumulator = "hola"
+  acumulator = ""
+  casa = nombre
+  primera = casa[0]
+primera = primera.toUpperCase()
+for (i = 1; i < casa.length ; i++){
+acumulator = acumulator + casa[i]
+}
+  return primera + acumulator
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  return cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb (n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +35,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var suma = 123
+  suma = 0
+  suma = numeros.reduce (function (acc, elemento) {
+return acc + elemento
+
+  })
+  cb(suma)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach (cb)
 }
 
 function map(array, cb) {
@@ -35,12 +56,28 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+
+  lala = array.map (cb)
+  return lala
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  words = array
+  
+
+  function eslaA (palabra) {
+    lala = palabra
+    return lala[0] == "a"
+  }
+  
+  falala = words.filter (eslaA)
+
+  apa = words[0][0]
+  
+  return falala
 }
 
 // No modificar nada debajo de esta línea
